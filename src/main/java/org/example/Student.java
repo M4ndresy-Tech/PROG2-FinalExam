29fa1b71cd1665a4057fa219c5a0174634ae39fa
Mandresy <m4ndresy@gmail.com>
2025-09-04 08:37:49 +0300

    feat: added the student file

---
+++ src/main/java/org/example/Student.java
@@ -0,0 +1,17 @@
+package org.example;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import java.util.Date;
+
+public class Student {
+    int id;
+    String firstName;
+    String lastName;
+    Date birthDate;
+    String email;
+    String phoneNumber;
+    String group;
+    String tutor;
+}
